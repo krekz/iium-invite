4d42a90a9d52563621de06651f0375811c70fa5c
krekz <kfitri559@gmail.com>
2024-11-02 20:58:52 +0800

    fix: replace the "view all" with chevron right icon

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import VerticalCarousel from "@/components/VerticalCarousel";
+import { ChevronRight } from "lucide-react";
 
 async function Home() {
 	const events = await prisma.event.findMany({
@@ -18,21 +19,21 @@ async function Home() {
 	return (
 		<main className="max-w-7xl mx-auto py-10 px-3">
 			<VerticalCarousel events={events} />
-			<div className="flex flex-col max-w-7xl mx-auto py-3 px-3">
+			<div className="flex flex-col max-w-7xl mx-auto py-7 px-3">
 				<div className="flex justify-between">
-					<h3 className="text-2xl">Upcoming Event</h3>
-					<Link href={"/discover"}>
-						<Button variant={"secondary"}>View All</Button>
+					<Link href={"/discover"} className="flex items-center gap-2">
+						<h3 className="text-2xl font-semibold">Upcoming Event</h3>
+						<ChevronRight className="h-5 w-5" />
 					</Link>
 				</div>
 				<EventCarousel events={events} />
 
 			</div>
-			<div className="flex flex-col max-w-7xl mx-auto py-3 px-3">
+			<div className="flex flex-col max-w-7xl mx-auto py-7 px-3">
 				<div className="flex justify-between">
-					<h3 className="text-2xl">Newly Added</h3>
-					<Link href={"/discover"}>
-						<Button variant={"secondary"}>View All</Button>
+					<Link href={"/discover"} className="flex items-center gap-2">
+						<h3 className="text-2xl font-semibold">Newly Added</h3>
+						<ChevronRight className="h-5 w-5" />
 					</Link>
 				</div>
 				<EventCarousel events={events} />
