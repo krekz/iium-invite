84eee4aefa037c129be85aec1df312eee9abc798
krekz <kfitri559@gmail.com>
2024-12-14 18:16:50 +0800

    added little events slug loading ui

--- src/app/(home)/events/[slug]/loading.tsx
+++ src/app/(home)/events/[slug]/loading.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 
 function Loading() {
     return (
-        <div className="max-w-screen-xl mx-auto px-4 py-8 text-white/85">
+        <div className="max-w-screen-xl mx-auto px-4 text-white/85">
+            <div className="h-10 bg-white/20 rounded w-1/2 mb-2"></div>
+            <div className="h-4 bg-white/20 rounded w-[10%]"></div>
             <div className="flex flex-col lg:flex-row gap-5 py-5">
                 {/* LEFT SIDE */}
                 <SkeletonLeft />
