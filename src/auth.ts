1537b8a6ac36035b89a123681368fdb4d1b4a902
krekz <kfitri559@gmail.com>
2024-12-20 16:34:02 +0800

    extend session object to get emailVerified value

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,40 @@
-import NextAuth from "next-auth"
+import NextAuth, { DefaultSession } from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import prisma from "@/lib/prisma"
 import google from "next-auth/providers/google"
 
+declare module "next-auth" {
+	interface Session {
+		user: {
+			/** The user's postal address. */
+			emailVerified: boolean
+			/**
+			 * By default, TypeScript merges new interface properties and overwrites existing ones.
+			 * In this case, the default session user properties will be overwritten,
+			 * with the new ones defined above. To keep the default session user properties,
+			 * you need to add them back into the newly declared interface.
+			 */
+		} & DefaultSession["user"]
+	}
+
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	adapter: PrismaAdapter(prisma),
 	providers: [google],
+
+	callbacks: {
+		session({ session, user }) {
+			return {
+				expires: session.expires,
+				user: {
+					id: user.id,
+					name: user.name,
+					email: user.email,
+					image: user.image,
+					emailVerified: user.emailVerified
+				}
+			}
+		},
+	}
 })
