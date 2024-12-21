0ac8437cdc700df2d80ff9153884b8fd9a74ce5f
krekz <kfitri559@gmail.com>
2024-12-22 01:42:40 +0800

    Revert "only apply session provider to page that really use. To avoid unnecessary api call"
    This reverts commit 0d1cd8245976fc99267e8ff55417916d578e320e.

--- src/app/(logged_in)/account/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SessionProvider } from "next-auth/react"
-
-export default function AccountLayout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return <SessionProvider>{children}</SessionProvider>
-}
