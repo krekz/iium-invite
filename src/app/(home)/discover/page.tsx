cc44ccdecd183fe5dcafc669e4017b720d7a042c
krekz <kfitri559@gmail.com>
2024-12-27 20:05:57 +0800

    improve performance due to "async" when navigate

--- src/app/(home)/discover/page.tsx
+++ src/app/(home)/discover/page.tsx
@@ -1,7 +1,7 @@
 import Filter from "@/components/discover/Filter";
 import EventList from "@/components/homepage/EventList";
 
-async function Events() {
+ function Events() {
 	return (
 		<div className="min-h-dvh w-full px-4 md:px-6 lg:px-8 py-3">
 			<div className="flex flex-col lg:flex-row gap-4">
