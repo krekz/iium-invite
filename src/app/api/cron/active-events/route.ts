06b8a8ec50ca0188cf907dc87b9c275156d74f6a
krekz <kfitri559@gmail.com>
2024-12-30 10:17:23 +0800

    fix vercel not execute cron route due to caching

--- src/app/api/cron/active-events/route.ts
+++ src/app/api/cron/active-events/route.ts
@@ -2,6 +2,8 @@ import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 import { NextRequest } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: NextRequest) {
 
   if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
