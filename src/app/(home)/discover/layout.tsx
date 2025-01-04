2fb44a47f9792994441f8bd545be3ea24856641a
krekz <kfitri559@gmail.com>
2025-01-04 13:47:55 +0800

    feat(discover): sidebar is now fixed on side

--- src/app/(home)/discover/layout.tsx
+++ src/app/(home)/discover/layout.tsx
@@ -6,7 +6,7 @@ function DiscoverLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="min-h-dvh w-full px-4 md:px-6 lg:px-8 py-3">
 			<div className="flex flex-col lg:flex-row lg:gap-4">
-				<aside className="w-full lg:w-64">
+				<aside className="w-full md:w-64 md:sticky md:top-36 md:h-fit">
 					<Filter />
 				</aside>
 				<div className="flex-1">
